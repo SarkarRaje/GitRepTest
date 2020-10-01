19ed685b8086d76519dba184797cd3aff545f885
Ashish <ashishgunjate9760@gmail.com>
2020-10-01 14:19:09 +0530

    add fun

--- GitwithXcode/ViewController.swift
+++ GitwithXcode/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func add1(n1:Int) -> Int
+    {
+        return n1*n1
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         print("Hello Arjun!")
+        print("Add1 : \(add1(n1: 12))")
     }
 
 
